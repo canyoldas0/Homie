f4b4755ac558bd328aecf357ff59ef93091e7b3f
Can Yoldas <canyoldas1997@gmail.com>
2024-05-25 19:28:55 +0200

    Initial Commit

---
+++ MyHome/MyHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHomeApp.swift
+//  MyHome
+//
+//  Created by Can Yoldas on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
